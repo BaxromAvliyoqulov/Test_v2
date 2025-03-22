c26e59fcf1e2cce1b3923cef5ef81463f0392471
BaxromAvliyoqulov <avliyoqulovbaxrom99@gmail.com>
2025-03-22 15:54:18 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <mainLayout>
+    <router-view />
+  </mainLayout>
+</template>
+
+<script>
+import MainLayout from '@/layout/mainLayout.vue'
+export default {
+  components: {
+    MainLayout
+  }
+}
+</script>
+
+<style></style>
---
+++ src/layout/mainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mainLayout">
+        <Navbar />
+        <div class="content">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar.vue'
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style></style>
